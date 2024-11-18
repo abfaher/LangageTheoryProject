26f45b046f26031e6b05b5b71e6473d1d6d1fb81
abfaher <abdoufaher11@gmail.com>
2024-11-18 13:00:25 +0100

    Reorganized repository into Part1 folder

---
+++ Part1/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Initialize the lexer
+            LexicalAnalyzer lexer = new LexicalAnalyzer(new FileReader(args[0]));
+            Symbol token;
+            LinkedHashMap<String, Integer> variables = new LinkedHashMap<String, Integer>(); // LinkedHashMap to preserve the order of the variables
+            // Read all tokens
+            while ((token = lexer.yylex()).getType() != LexicalUnit.EOS) {
+                System.out.println(token.toString());
+                if(token.getType() == LexicalUnit.VARNAME && !variables.containsKey(token.getValue())) {
+                    variables.put(token.getValue().toString(), token.getLine());
+                }
+            }
+            System.out.println("\nVariables");
+            for (String key : variables.keySet()) {
+                System.out.println(key + "\t" + variables.get(key));
+            }
+        }
+        catch (IOException e) {
+            System.err.println("An I/O error occured while reading the input: " + e.getMessage());
+        } 
+    }
+}
