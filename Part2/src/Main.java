0708981d50d3dda5f8104974abd2929c64d724d9
edenarrr <eden.aroch@gmail.com>
2024-11-24 22:53:46 +0100

    arguments parsed better and makefile done

--- Part2/src/Main.java
+++ Part2/src/Main.java
@@ -4,20 +4,37 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try {
-            if (args.length < 1) {
-                System.err.println("Usage: java Main <input file> [-wt <output file>]");
+            if (args.length < 1 || args.length > 3) {
+                System.err.println("Usage: java Main [-wt <output file>] <input file>");
+                return;
+            }
+
+            String inputFile;
+            String outputFile = null;
+
+            // Check if we have the optional -wt argument
+            if (args.length == 3) {
+                if (!args[0].equals("-wt")) {
+                    System.err.println("Usage: java Main [-wt <output file>] <input file>");
+                    return;
+                }
+                outputFile = args[1];
+                inputFile = args[2];
+            } else if (args.length == 1) {
+                inputFile = args[0];
+            } else {
+                System.err.println("Usage: java Main [-wt <output file>] <input file>");
                 return;
             }
 
             // Create the lexical analyzer and a parser
-            LexicalAnalyzer lexer = new LexicalAnalyzer(new FileReader(args[0]));
+            LexicalAnalyzer lexer = new LexicalAnalyzer(new FileReader(inputFile));
             Parser parser = new Parser(lexer);
 
             // Parse the program and generate the parse tree
             parser.parseProgram();
 
-            if (args.length == 3 && args[1].equals("-wt")) {
-                String outputFile = args[2];
+            if (outputFile != null) {
                 parser.parseTree.writeParseTreeToLaTeX(outputFile);
             } else {
                 // Print the derivation if no LaTex output is requested
